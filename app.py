635ad17093ed6dd4acd70d4f1fffa20b7679e53a
singh-ashok25 <62803698+singh-ashok25@users.noreply.github.com>
2020-03-28 16:05:05 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask,  escape, request
+app = Flask(__name__)
+
+@app.route("/")
+
+def main():
+    return "Welcome to Devops Course from - Ashok Singh !"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=80)
