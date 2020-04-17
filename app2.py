2263a5d9a830b1014e12a7466fe9060bdc2ff525
singh-ashok25 <ashok_iitr@yahoo.com>
2020-04-17 23:00:06 +0100

    fixing port back to 4080

--- app2.py
+++ app2.py
@@ -19,4 +19,4 @@ def tushar_new():
 #    return render_template('surya_new.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=4082)
+    app.run(debug=True, host="0.0.0.0", port=4080)
