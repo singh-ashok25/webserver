54575e7b6c2c5a0268cb941b180d9fa2e478281e
dj <dharamjitbiswal@gmail.com>
2021-01-30 22:35:52 +0530

    changes

--- app2.py
+++ app2.py
@@ -10,13 +10,9 @@ def main():
 def about():
     return render_template('about.html')
 
-@app.route("/tushar_new.html")
-def tushar_new():
-    return render_template('tushar_new.html')
-
-@app.route("/surya_new.html")
-def surya_new():
-    return render_template('surya_new.html')
+@app.route("/djnew.html")
+def djnew():
+    return render_template('djnew.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=4080)
